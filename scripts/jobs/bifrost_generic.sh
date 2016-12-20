8f9f38ed71d2b99f87e767e50aefab2624aa0cda
Ingwarr <sorceress1979@gmail.com>
2016-12-20 22:38:05 +0200

    var declared

--- scripts/jobs/bifrost_generic.sh
+++ scripts/jobs/bifrost_generic.sh
@@ -66,10 +66,9 @@ function main {
 	 DNSMASQ_ROUTER="192.168.122.1"
          LIBVIRTD_ENABLED=1
          NET_IF="virbr0"
-         num=0
 	 execute_ssh_cmd ${env_ip} root r00tme "apt -y install qemu-kvm libvirt-bin virtinst; exit"
 	 sshpass -p 'r00tme' scp -r ${scp_opts} $HELPERS_DIR/vm/ps_bm.xml root@${env_ip}:/etc/libvirt/qemu/
-	 execute_ssh_cmd ${env_ip} root r00tme "qemu-img create -f qcow2 /var/lib/libvirt/images/ps_bm.qcow2 15G; virsh define /etc/libvirt/qemu/ps_bm.xml && for ((num=1; num<=3; num++)); do virt-clone -o ps_bm -n ps_bm-${i} --auto-clone; done "
+	 execute_ssh_cmd ${env_ip} root r00tme "qemu-img create -f qcow2 /var/lib/libvirt/images/ps_bm.qcow2 15G; num=0; virsh define /etc/libvirt/qemu/ps_bm.xml && for ((num=1; num<=3; num++)); do virt-clone -o ps_bm -n ps_bm-${i} --auto-clone; done "
   fi
 mkdir -p /tmp/${ENV_NAME}/
   echo "---
